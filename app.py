5b6d3bd04946303a9071cb9af2b3f6de9e36a2cc
Sarah <sjbrooks305@gmail.com>
2020-03-30 15:41:53 -0400

    finished part 2 and tested with insomnia; haven't run our tests yet

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 """Flask app for Cupcakes"""
 
-from flask import Flask, render_template, redirect, flash
-
+from flask import Flask, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
-
 from models import db, connect_db, Cupcake
 # from flask_wtf import FlaskForm
 # from forms import AddPetForm
@@ -25,3 +23,58 @@ db.create_all()
 
 toolbar = DebugToolbarExtension(app)
 
+
+def serialize_cupcake(cupcake):
+    """Serialize a cupcake SQLAlchemy object to a dictionary"""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image,
+    }
+
+@app.route('/api/cupcakes')
+def list_all_cupcakes():
+    """Get data about all cupcakes in JSON
+    {'cupcakes': [{id, flavor, size, rating, image}...]}."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.route('/api/cupcakes/<int:id>')
+def list_single_cupcake(id):
+    """Get data about a single cupcakes in JSON
+    {'cupcake': {id, flavor, size, rating, image}}."""
+
+    cupcake = Cupcake.query.get_or_404(id)
+    serialized = serialize_cupcake(cupcake)
+
+    return jsonify(cupcake=serialized)
+
+@app.route('/api/cupcakes', methods=['POST'])
+def add_cupcake():
+    """Add a new cupcake to the database using post request JSON sent.
+    Respond with JSON {cupcake: {id, flavor, size, rating, image}}"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    # how does it know which falsey value to default to?
+    image = request.json["image"] or None
+
+    new_cupcake = Cupcake(flavor=flavor, 
+                          size=size, 
+                          rating=rating, 
+                          image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = serialize_cupcake(new_cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
+    # how do we remove possibility of duplication
